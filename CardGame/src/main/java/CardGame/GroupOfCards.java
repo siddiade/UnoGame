bf4285326052c05484a485b01a9a806380715545
siddiade <50712861+siddiade@users.noreply.github.com>
2020-06-24 23:56:29 -0400

    Update GroupOfCards.java

--- CardGame/src/main/java/CardGame/GroupOfCards.java
+++ CardGame/src/main/java/CardGame/GroupOfCards.java
@@ -15,31 +15,44 @@ import java.util.Collections;
  * @author dancye
  */
 public class GroupOfCards 
-{
-   
-    //The group of cards, stored in an ArrayList
-    private ArrayList <Card> cards;
+ //The group of cards, stored in an ArrayList
+    private ArrayList<String> cards;
     private int size;//the size of the grouping
-    
+
     public GroupOfCards(int givenSize)
     {
+        Random rand = new Random();
         size = givenSize;
-        cards = new ArrayList <Card>();
+        cards = new ArrayList<String>();
         String[] colours = {"red", "blue", "green", "yellow"};
+        String colour;
         int[] number = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int num;
 
-        
+        int randGenCol = rand.nextInt(colours.length);
+        int randGenNum = rand.nextInt(number.length);
+
+
+        for(int i=0; i<size; i++) {
+            num = number[randGenNum];
+            colour = colours[randGenCol];
+            for (i=0; i<size; i++) {
+                cards.add(num + colour);
+                System.out.println(cards.get(i));
+
+            }
+        }
     }
-    
+
     /**
      * A method that will get the group of cards as an ArrayList
      * @return the group of cards.
      */
-    public ArrayList<Card> showCards()
+    public ArrayList<String> showCards()
     {
         return cards;
     }
-    
+
     public void shuffle()
     {
         Collections.shuffle(cards);
@@ -58,5 +71,9 @@ public class GroupOfCards
     public void setSize(int givenSize) {
         size = givenSize;
     }
-    
+
+    public static void main(String args[]) {
+        new GroupOfCards(2);
+    }
+
 }//end class
